113dbdbf6d4e5a980e3b97be61f66bd7fb304c4a
toggi0991 <60846780+toggi0991@users.noreply.github.com>
2021-10-28 17:01:28 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-document.write("Hello world");
+document.write("<h1>Hello World</h1>");
+
+function add_column(){
+  let coldiv = document.getElementById("coldiv"+ColumnNumber);
+  let ColumnNumber = 1;
+  ++ColumnNumber;
+  let newdiv = document.createElement('div');
+  newdiv.setAttribute("id", "div" + String(ColumnNumber));
+  let newinput = document.createElement('input');
+  newinput.setAttribute("type","text");
+  newinput.setAttribute("name", "input" + String(ColumnNumber));
+  newdiv.appendChild(newinput);
+  coldiv.appendChild(newdiv);
+}
